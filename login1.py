eea4ef4395769c3fb8a0c7829ac008e1e2708350
Richard Ardila <richardardilaz17@hotmail.com>
2023-10-15 02:06:02 -0400

    modification of the instructions, modification of the login, only the function remains, modification of the user_register file name.

--- login1.py
+++ login1.py
@@ -1,21 +1,8 @@
-# Una vez se cumpla la condicion del else debe abortar proceso o detenerse.
-# Crear condiciones de password igual que el usuario, validar que sea correcta.
-# Crear condicional regex
-# Modificar que validacion de user and password debe ser en base de datos.
-
-def main():
-    login()
-
-
 def login():
     print("Login")
     name = input("Enter your username: ").strip()
     pass_word = input("Enter your password: ").strip()
-    print(f"Login Successful! {name},{pass_word}")
-
-
-if __name__ == "__main__":
-    main()
+    print(f"Login Successful! {name}, {pass_word}")
 
 
     
---
+++ main.py
@@ -0,0 +1,9 @@
+from login1 import login
+from user_register import user_register
+
+
+# Enter the logic for login validation.
+login()
+
+# Enter the logic for user registration.
+user_register()
